91df6793b4fac61eb6543ada4c1e8a6c41a714b7
hjy009 <hjy009@163.com>
2015-09-28 21:06:25 +0800

    dao

---
+++ src/main/java/fuzhu/gwee/passport/PassportDAO.java
@@ -0,0 +1,18 @@
+package fuzhu.gwee.passport;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONStringer;
+
+public class PassportDAO {
+	public Map<String,Passport> passports = new HashMap<String,Passport>();
+	public JSONArray Load(String filename){
+		JSONStringer jstr = new JSONStringer();
+
+		
+		return null;
+	}
+	
+}

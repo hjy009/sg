de158f223fe3d20f5768dd5bdde4446d1d2d7959
hjy009 <hjy009@163.com>
2015-09-29 18:45:10 +0800

    sg

---
+++ src/main/java/fuzhu/gwee/passport/City.java
@@ -0,0 +1,17 @@
+package fuzhu.gwee.passport;
+
+
+
+public class City {
+	char type='o';
+	int num = 4;
+	String message = null;
+	int error = 0;
+	int[] depot = {0,0,0,0};
+	int[] user_base_max = {5120,5120,5120,5120};
+	int[] user_base_per = {48,48,48,48};
+	int[] consumption = {72,14,4};
+	double general_append = 1.75;
+	int green_server = 1;
+	String key ;
+}

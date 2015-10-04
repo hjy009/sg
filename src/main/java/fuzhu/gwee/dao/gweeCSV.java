ca180fba5ea3c8abc121c4902c0d35021d8f1387
hjy009 <hjy009@163.com>
2015-10-04 16:13:57 +0800

    objectdb

--- src/main/java/fuzhu/gwee/dao/gweeCSV.java
+++ src/main/java/fuzhu/gwee/dao/gweeCSV.java
@@ -2,16 +2,21 @@ package fuzhu.gwee.dao;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.Reader;
+import java.io.Writer;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
 public class gweeCSV {
 
 	public void readpassport(){
 		Reader in;
+		Writer out;
 		try {
 			in = new FileReader("res/passport.csv");
 			Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(in);
@@ -20,6 +25,8 @@ public class gweeCSV {
 			    String password = record.get("password");
 			    System.out.println(username+":"+password);
 			}		
+			out = new FileWriter("res/passport.csv");
+			CSVPrinter printer = CSVFormat.DEFAULT.withHeader("H1", "H2").print(out);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
